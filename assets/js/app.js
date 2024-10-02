9026619d323573dbc53685e1ce5e07511904cb1f
Gwilym  Berrow <gwilymberrow2000@gmail.com>
2024-10-02 13:55:39 +0000

    Installed stockfish (powerful ai engine)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,6 @@
+// Stockfish worker
+const stockfish = new Worker('assets/js/stockfish-16.1.js');
+                              
 const initialBoardSetup = {
     a8: 'rook-black', b8: 'knight-black', c8: 'bishop-black', d8: 'queen-black', e8: 'king-black', f8: 'bishop-black', g8: 'knight-black', h8: 'rook-black',
     a7: 'pawn-black', b7: 'pawn-black', c7: 'pawn-black', d7: 'pawn-black', e7: 'pawn-black', f7: 'pawn-black', g7: 'pawn-black', h7: 'pawn-black',
