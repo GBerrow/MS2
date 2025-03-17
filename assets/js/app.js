3902d0b9b922a66277c32c0a37ba1e6e846a7663
GBerrow <gwilymberrow2000@gmail.com>
2025-03-17 22:27:30 +0000

    Refactor app.js to initialize board on DOM load

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+// assets/js/app.js
+import { initializeBoard } from './board/board-setup.js';
+
+// Initialize the game when the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', () => {
+    initializeBoard();
+});
